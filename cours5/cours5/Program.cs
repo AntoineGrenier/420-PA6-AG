b3a2df2df9a224d911b4362e00529cd6a64fd7d5
Antoine Grenier <antoine.grenier3@gmail.com>
2025-09-24 18:55:19 -0400

    Ajout cours 5, 2 premiers exervcices

--- cours5/cours5/Program.cs
+++ cours5/cours5/Program.cs
@@ -1,13 +1,42 @@
-﻿static class Program
+﻿using Microsoft.Data.SqlClient;
+
+static class Program
 {
     static void Main(string[] args)
     {
+        string connStr = "Server=TOUR-ANTOINE;Database=MaBase;Trusted_Connection=True;TrustServerCertificate=true;";
+        using (SqlConnection con = new SqlConnection(connStr))
+        {
+            con.Open();
+            Console.WriteLine("Connexion réussie");
+            var strQuery = "SELECT * FROM Clients";
+            SqlCommand cmd = new SqlCommand(strQuery, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"ID: {reader["Id"]}, Nom: {reader["Nom"]}, Email: {reader["Email"]}");
+            }
+        }
+
         //Exercice 1 – Connexion à la base
         //Objectif: Vérifier la connexion à SQL Server depuis C#.
         //Énoncé:
         //Créer une base MaBase dans SSMS;
         //Écrire un programme C# qui ouvrir une connexion et afficher "Connexion réussie" si tout fonctionne.
         Console.WriteLine("|*************************Exercice #1*************************|");
+        var strConnectionString = "Server = {VOTRE_SERVEUR};Database = {VOTRE_BASE}; Trusted_Connection = True; TrustServerCertificate = true; ";
+        using (SqlConnection connection = new SqlConnection(strConnectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connexion réussie");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur de connexion: {ex.Message}");
+            }
+        }
 
         //Exercice 2 – Lecture des données
         //Objectif : Lire et afficher des données avec SqlDataReader.
@@ -15,6 +44,17 @@
         //Dans SSMS, créer une table Clients avec Id, Nom, Email et insérer quelques lignes;
         //Écrire un programme C# qui lit et afficher tous les clients.
         Console.WriteLine("|*************************Exercice #2*************************|");
+        var strQuery2 = "SELECT * FROM Clients";
+        using (SqlConnection connection = new SqlConnection(strConnectionString))
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand(strQuery2, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"ID: {reader["Id"]}, Nom: {reader["Nom"]}, Email: {reader["Email"]}");
+            }
+        }
 
         //Exercice 3 – Insertion de données
         //Objectif : Ajouter un enregistrement depuis C#.
